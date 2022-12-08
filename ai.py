cc138205fa39e2303d5f0d9843f091e3417cf539
Xeu <thankrain@qq.com>
2022-12-08 23:02:30 +0800

    Init Commit

---
+++ ai.py
@@ -0,0 +1,276 @@
+import random
+import time
+from typing import List
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import float32
+
+from client.base import SlaveWeaponType
+from client.resp import PacketResp, Character, Block, Item, BuffType, SlaveWeapon
+
+# Hyper Parameters
+BATCH_SIZE = 32
+LR = 0.01  # 学习率
+EPSILON = 0.95  # 贪心策略
+GAMMA = 0.8  # 奖励衰减
+TARGET_REPLACE_ITER = 100  # 目标更新频率
+MEMORY_CAPACITY = 5000  # 记忆容量
+ACTION_SHAPE = (9, 3, 6)
+N_ACTIONS = (7 + 3) * 3 + 6  # 操作数 (方向6 + 无操作+隐身3) + (Move + 攻击 * 2) + 排序(6) = 36
+N_STATES = 16 * 16 * 1 + 1 + 17 * 2  # 状态数 = 地图信息 16*16 + 当前击杀 + 两个角色的17个属性 = 291
+
+
+# N_STATES * N_ACTIONS = 10476
+
+class DQN(object):  # 强化神经网络
+    def __init__(self, id):
+        self.eval_net, self.target_net = Net(), Net()  # 定义两个网络：评估网络 & 目标网络
+        self.learn_step_counter = 0  # 用于目标网络延迟更新
+        self.memory_counter = 0  # 存储计数器
+        self.epsilon_decay_rate = 0.9995
+        self.epsilon_min_rate = 0.1
+        self.epsilon = 1.0
+        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 1 + N_ACTIONS))  # 初始化记忆全为 0
+        # 记忆单元为 当前状态 行为 奖励 操作后状态
+        # 单元数为 状态维度 * 2 + 行为维度 + 奖励维度(1)
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)  # todo Adam 优化器
+        self.loss_func = nn.MSELoss()  # 均方误差损失函数
+        # 读取记忆
+        try:
+            self.memory = np.loadtxt("memory" + id + ".txt", delimiter=',')
+            valid = len(self.memory[np.any(self.memory != 0, axis=1)])
+            print("[有效记忆]", valid)
+            self.memory_counter = valid
+            print("加载记忆成功")
+        except Exception as e:
+            print("加载记忆失败:", e)
+
+    def choose_action(self, x):  # 传入当前的 State，计算行为
+        # x = torch.unsqueeze(torch.IntTensor(x), 0)  # 解包
+        # 只输入一个样本
+        if self.memory_counter >= MEMORY_CAPACITY and np.random.uniform() < self.epsilon:  # 90% 概率使用评估网络的结果
+            actions_value = self.eval_net.forward(x)  # 使用评估网络获取行为
+            # actions_value = [a1,b1,c1,d1]
+            # actions_value = [0..8,9..11,12..17]
+            direction_move = torch.max(actions_value[:7], 0).indices.data.item()  # 7行的最大值索引
+            sneaky_move = torch.max(actions_value[7:10], 0).indices.data.item()  # 3行的最大值索引
+            direction_master = torch.max(actions_value[10:17], 0).indices.data.item()  # 7行的最大值索引
+            sneaky_master = torch.max(actions_value[17:20], 0).indices.data.item()  # 3行的最大值索引
+            direction_slave = torch.max(actions_value[20:27], 0).indices.data.item()  # 7行的最大值索引
+            sneaky_slave = torch.max(actions_value[27:30], 0).indices.data.item()  # 3行的最大值索引
+            rank = torch.max(actions_value[30:], 0).indices.data.item()  # 6行的最大值索引 len = 36
+            # 返回argmax索引
+        else:  # 随机
+            direction_move = random.randint(0, 5)
+            sneaky_move = random.randint(0, 1)
+            direction_master = random.randint(0, 5)
+            sneaky_master = random.randint(0, 1)
+            direction_slave = random.randint(0, 5)
+            sneaky_slave = random.randint(0, 1)
+            rank = random.randint(0, 4)
+        print(
+            f"{direction_move}, {sneaky_move}, {direction_master}, {sneaky_master}, {direction_slave}, {sneaky_slave}, {rank}")
+        self.epsilon *= self.epsilon_decay_rate
+        self.epsilon = max(self.epsilon, self.epsilon_min_rate)
+        return direction_move, sneaky_move, direction_master, sneaky_master, direction_slave, sneaky_slave, rank
+
+    def store_transition(self, s, a, r, s_):
+        # print('[state]:',s)
+        # print('[action,reward]:',[a, r])
+        # print('[stated]:',s_)
+        if s is None or a is None or s_ is None:
+            return
+        v = (*[i.item() for i in s], *list(a), r, s_)
+        # print("\n", "Store: action:", len(a))
+        transition = np.hstack(v)
+        # 用新内存替换旧内存
+        index = self.memory_counter % MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
+    def learn(self):
+        # 目标参数更新
+        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+        self.learn_step_counter += 1
+
+        # 样本批次转换
+        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
+        b_memory = self.memory[sample_index, :]
+        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
+        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES + N_ACTIONS].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, N_STATES + N_ACTIONS:N_STATES + N_ACTIONS + 1])
+        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
+
+        # q_eval w.r.t体验中的行动
+        q_eval_temp = self.eval_net(b_s)
+        # print("[Q_Eval]:", q_eval_temp)
+        # print("[Batcg_action]:", b_a.shape)
+        # torch.set_printoptions(profile="full")
+        # print("[Batcg_action]:", b_a)
+        q_eval = q_eval_temp.gather(1, b_a)  # shape (batch, 1)
+        q_next = self.target_net(b_s_).detach()  # detach from graph, don't backpropagate
+        q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)  # shape (batch, 1)
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def save(self, id):
+        # print("\n\n[Memory]\n", self.memory)
+        np.savetxt('memory' + id + '.txt', self.memory, fmt="%d", delimiter=',')
+
+
+class Net(nn.Module):
+    def __init__(self, ):
+        super(Net, self).__init__()
+        # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # 单GPU或者CPU
+        # print("[Device]", device)
+        self.fc1 = nn.Linear(N_STATES, N_STATES * N_ACTIONS)  # .to(device)  # 传入网络 状态数 -> 状态 * 行动
+        self.fc1.weight.data.normal_(0, 0.1)  # 随机初始化权重
+        self.out = nn.Linear(N_ACTIONS * N_STATES, N_ACTIONS)  # .to(device)  # 输出网络 状态 * 行动 -> 输出Action数
+        self.out.weight.data.normal_(0, 0.1)  # 随机初始化权重
+
+    def forward(self, x):  # 前向传播
+        x = self.fc1(x)  # 第一层映射
+        x = F.relu(x)  # 激活函数 线性整流函数 f(x) = max(0,x)
+        actions_value = self.out(x)  # 输出映射 [p1,p2,p3,p4,...,pn]
+        return actions_value
+
+
+class AI(object):
+    def __init__(self, id):
+        self.last_resp = None
+        self.last_state = None
+        self.last_action = None
+        self.times = 0
+        self.ep_r = 0
+        self.start = time.time()
+        self.dqn = DQN(id)
+        try:
+            print("读取模型中...")
+            self.dqn.target_net = torch.load("target-" + id + ".pth")
+            self.dqn.eval_net = torch.load("eval-" + id + ".pth")
+            print("读取模型成功!")
+        except Exception as _:
+            print("读取模型失败!")
+
+    def get_action(self, resp):  # 获取Action
+        state = state_trans(resp)
+        action = self.dqn.choose_action(state)
+        # direction_move = torch.max(actions_value[:7], 0).indices.data.item()  # 7行的最大值索引
+        #             sneaky_move = torch.max(actions_value[7:10], 0).indices.data.item()  # 3行的最大值索引
+        #             direction_master = torch.max(actions_value[10:17], 0).indices.data.item()  # 7行的最大值索引
+        #             sneaky_master = torch.max(actions_value[17:20], 0).indices.data.item()  # 3行的最大值索引
+        #             direction_slave = torch.max(actions_value[20:27], 0).indices.data.item()  # 7行的最大值索引
+        #             sneaky_slave = torch.max(actions_value[27:30], 0).indices.data.item()  # 3行的最大值索引
+        #             rank = torch.max(actions_value[30:], 0).indices.data.item()  # 6行的最大值索引 len = 36
+        last_action = np.zeros(36, dtype=int)
+        last_action[action[0]] = 1
+        last_action[7 + action[1]] = 1
+        last_action[10 + action[2]] = 1
+        last_action[17 + action[3]] = 1
+        last_action[20 + action[4]] = 1
+        last_action[27 + action[5]] = 1
+        last_action[30 + action[6]] = 1
+        self.last_action = last_action
+        self.last_state = state
+        self.last_resp = resp
+        return action
+
+    def resp(self, resp_):  # 存储响应
+        reward = self.reward()
+        state_ = state_trans(resp_)
+        # print("Save Response")
+        self.dqn.store_transition(self.last_state, self.last_action, reward, state_)
+        self.times += 1
+        self.ep_r += reward
+        print("[Memory]", self.dqn.memory_counter, "/", MEMORY_CAPACITY)
+        if self.dqn.memory_counter > MEMORY_CAPACITY:
+            print("Learn")
+            self.dqn.learn()
+
+    def done(self, episode, score):
+        t = time.time() - self.start
+        print('轮数: ', episode,
+              '| 次数: ', round(self.times, 2),
+              '| 奖励: ', round(self.ep_r, 2),
+              '| 结果: ', round(score, 4),
+              '| 时间: ', round(t, 2), 's'
+              )
+
+    def reward(self):
+        try:
+            return self.last_resp.data.kill * 10 + self.last_resp.data.score - self.ep_r
+        except Exception as e:
+            return 0.0
+
+    def save(self, id):
+        print("保存模型中...")
+        torch.save(self.dqn.target_net, "target-" + id + ".pth")
+        torch.save(self.dqn.eval_net, "eval-" + id + ".pth")
+        print("保存记忆中")
+        self.dqn.save(id)
+        print("保存成功，程序已退出")
+
+
+def state_trans(state: PacketResp):
+    me = state.data.characters[0]
+    other = Character()
+    for block in state.data.map.blocks:
+        for obj in block.objs:
+            if isinstance(obj, Character):
+                assert isinstance(obj, Character)
+                if obj.characterID != me.characterID:
+                    other = obj
+                    break
+    # 16 * 16 +  1 + (5 + 6 + 3 + 3) * 2 = 35
+    tup = block_map(state.data.map.blocks) + [state.data.kill,
+                                              me.x, me.y, me.direction.value, me.hp, me.moveCD,
+                                              me.moveCDLeft, me.isAlive, me.isSneaky, me.isGod, me.rebornTimeLeft,
+                                              me.godTimeLeft,
+                                              me.masterWeapon.weaponType.value, me.masterWeapon.attackCD,
+                                              me.masterWeapon.attackCDLeft,
+                                              me.slaveWeapon.weaponType.value, me.slaveWeapon.attackCD,
+                                              me.slaveWeapon.attackCDLeft,
+                                              other.x, other.y, other.direction.value, other.hp, other.moveCD,
+                                              other.moveCDLeft, other.isAlive, other.isSneaky,
+                                              other.isGod, other.rebornTimeLeft, other.godTimeLeft,
+                                              other.masterWeapon.weaponType.value, other.masterWeapon.attackCD,
+                                              other.masterWeapon.attackCDLeft,
+                                              other.slaveWeapon.weaponType.value, other.slaveWeapon.attackCD,
+                                              other.slaveWeapon.attackCDLeft]
+    return torch.tensor(tup, dtype=float32)
+
+
+def block_map(blocks: List[Block]):
+    li = []
+    for i in range(16 * 16):
+        if len(blocks) > i:
+            block = blocks[i]
+            v = 0
+            if len(block.objs) > 0:
+                obj = block.objs[-1]
+                if obj is Item:
+                    assert isinstance(obj, Item)
+                    if obj.buffType == BuffType.BuffHp:
+                        v = 1
+                    else:
+                        v = 2
+                elif obj is SlaveWeapon:
+                    assert isinstance(obj, SlaveWeapon)
+                    if obj.weaponType == SlaveWeaponType.Kiwi:
+                        v = 3
+                    else:
+                        v = 4
+            if block.valid:
+                li.append(block.color.value + v * 10)
+            else:
+                li.append(-1)
+        else:
+            li.append(0)
+    return li
---
+++ main.py
@@ -0,0 +1,253 @@
+import copy
+import logging
+import socket
+import sys
+import time
+from itertools import cycle
+from threading import Thread
+from time import sleep
+
+from client.ai import AI
+from config import config
+from req import *
+from resp import *
+
+# logger config
+logging.basicConfig(
+    # uncomment this will redirect log to file *client.log*
+    # filename="client.log",
+    format="[%(asctime)s][%(levelname)s] %(message)s",
+    filemode="a+",
+)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# record the context of global data
+gContext = {
+    "playerID": None,
+    "characterID": [],
+    "gameOverFlag": False,
+    "prompt": (
+        "Take actions!\n"
+        "'s': move in current direction\n"
+        "'w': turn up\n"
+        "'e': turn up right\n"
+        "'d': turn down right\n"
+        "'x': turn down\n"
+        "'z': turn down left\n"
+        "'a': turn up left\n"
+        "'u': sneak\n"
+        "'i': unsneak\n"
+        "'j': master weapon attack\n"
+        "'k': slave weapon attack\n"
+        "Please complete all actions within one frame! \n"
+        "[example]: sdq\n"
+    ),
+    "steps": ["-", "\\", "|", "/"],
+    "gameBeginFlag": False,
+}
+
+
+class Client(object):
+    """Client obj that send/recv packet.
+
+    Usage:
+        >>> with Client() as client: # create a socket according to config file
+        >>>     client.connect()     # connect to remote
+        >>> 
+    """
+
+    def __init__(self) -> None:
+        self.config = config
+        self.host = self.config.get("Host")
+        self.port = self.config.get("Port")
+        assert self.host and self.port, "host and port must be provided"
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        if self.socket.connect_ex((self.host, self.port)) == 0:
+            logger.info(f"connect to {self.host}:{self.port}")
+        else:
+            logger.error(f"can not connect to {self.host}:{self.port}")
+            exit(-1)
+        return
+
+    def send(self, req: PacketReq):
+        msg = json.dumps(req, cls=JsonEncoder).encode("utf-8")
+        length = len(msg)
+        self.socket.sendall(length.to_bytes(8, sys.byteorder) + msg)
+        # uncomment this will show req packet
+        # logger.info(f"send PacketReq, content: {msg}")
+        return
+
+    def recv(self):
+        length = int.from_bytes(self.socket.recv(8), sys.byteorder)
+        result = b''
+        while resp := self.socket.recv(length):
+            result += resp
+            length -= len(resp)
+            if length <= 0:
+                break
+
+        # uncomment this will show resp packet
+        # logger.info(f"recv PacketResp, content: {result}")
+        packet = PacketResp().from_json(result)
+        return packet
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.socket.close()
+        if traceback:
+            print(traceback)
+            return False
+        return True
+
+
+def cliGetInitReq():
+    """Get init request from user input."""
+    masterWeaponType = 2  # input("Make choices!\nmaster weapon type: [select from {1-2}]: ")
+    slaveWeaponType = 1  # input("slave weapon type: [select from {1-2}]: ")
+    return InitReq(
+        MasterWeaponType(int(masterWeaponType)), SlaveWeaponType(int(slaveWeaponType))
+    )
+
+
+def cliGetActionReq(characterID: int, ai: AI, resp) -> []:
+    direction2action = {
+        0: (ActionType.TurnAround, TurnAroundActionParam(Direction.Above)),
+        1: (ActionType.TurnAround, TurnAroundActionParam(Direction.TopRight)),
+        2: (ActionType.TurnAround, TurnAroundActionParam(Direction.BottomRight)),
+        3: (ActionType.TurnAround, TurnAroundActionParam(Direction.Bottom)),
+        4: (ActionType.TurnAround, TurnAroundActionParam(Direction.BottomLeft)),
+        5: (ActionType.TurnAround, TurnAroundActionParam(Direction.TopLeft)),
+    }
+    sneaky2action = {
+        0: (ActionType.Sneaky, EmptyActionParam()),
+        1: (ActionType.UnSneaky, EmptyActionParam()),
+    }
+
+    def d2action(d):
+        if d < len(direction2action):
+            return ActionReq(characterID, *direction2action[d])
+
+    def s2action(s):
+        if s < len(sneaky2action):
+            return ActionReq(characterID, *sneaky2action[s])
+
+    direction_move, sneaky_move, direction_master, sneaky_master, direction_slave, sneaky_slave, rank = ai.get_action(
+        resp)
+
+    move = [d2action(direction_move), s2action(sneaky_move),
+            ActionReq(characterID, ActionType.Move, EmptyActionParam())]
+    master = [d2action(direction_master), s2action(sneaky_master),
+              ActionReq(characterID, ActionType.MasterWeaponAttack, EmptyActionParam())]
+    slave = [d2action(direction_slave), s2action(sneaky_slave),
+             ActionReq(characterID, ActionType.SlaveWeaponAttack, EmptyActionParam())]
+
+    # (9,3,6) = (方向+隐身/解除隐身/无操作) * (Move,主武器攻击,副武器攻击)*排序
+    # rank 排序
+    # 0 -> move M S
+    # 1 -> move S M
+    # 2 -> M move S
+    # 3 -> M S move
+    # 4 -> S move M
+    # 5 -> S M move
+
+    rank2actions = {
+        0: move + master + slave,
+        1: move + slave + master,
+        2: master + move + slave,
+        3: master + slave + move,
+        4: slave + move + master,
+        5: slave + master + move
+    }
+
+    actionReqs = get_real_arr(rank2actions[rank])
+    # print("[Action]", actionReqs)
+    return actionReqs
+
+
+def get_real_arr(arr):
+    """
+    返回删除所有空值后的arr
+    """
+    arr_copy = copy.deepcopy(arr)
+    arr_copy = list(filter(None, arr_copy))
+    while '' in arr_copy:
+        arr_copy.remove('')
+    return arr_copy
+
+
+def recvAndRefresh(client: Client, ai: AI, id):
+    """Recv packet and refresh ui."""
+    global gContext
+    resp = client.recv()
+    if resp.type == PacketType.ActionResp:
+        if len(resp.data.characters) and not gContext["gameBeginFlag"]:
+            gContext["characterID"] = resp.data.characters[-1].characterID
+            gContext["playerID"] = resp.data.playerID
+            gContext["gameBeginFlag"] = True
+
+    # print("[CTX]", gContext)
+    frame = 0
+    while resp.type != PacketType.GameOver:
+        t = time.time()
+        if gContext["characterID"] is not None:
+            # print("[resp]",resp)
+            if action := cliGetActionReq(gContext["characterID"], ai=ai, resp=resp):
+                actionPacket = PacketReq(PacketType.ActionReq, action)
+                client.send(actionPacket)
+        resp = client.recv()
+        t1 = time.time()
+        t2 = time.time()
+        print("\n[决策时间]", int((t1 - t) * 1000), "ms [响应时间]",
+              int((t2 - t1) * 1000), "ms \n[总时间]", int((t2 - t) * 1000), "ms\n")
+
+    print(f"Game Over!")
+    for (idx, score) in enumerate(resp.data.scores):
+        if gContext["playerID"] == idx:
+            print(f"You've got \33[1m{score} score\33[0m")
+        else:
+            print(f"The other player has got \33[1m{score} score \33[0m")
+
+    if resp.data.result == ResultType.Win:
+        print("\33[1mCongratulations! You win! \33[0m")
+    elif resp.data.result == ResultType.Tie:
+        print("\33[1mEvenly matched opponent \33[0m")
+    elif resp.data.result == ResultType.Lose:
+        print(
+            "\33[1mThe goddess of victory is not on your side this time, but there is still a chance next time!\33[0m"
+        )
+
+    gContext["gameOverFlag"] = True
+    print("Press any key to exit......")
+
+
+def main(id):
+    ai = AI(id)
+    with Client() as client:
+        client.connect()
+        initPacket = PacketReq(PacketType.InitReq, cliGetInitReq())
+        client.send(initPacket)
+        print(gContext["prompt"])
+        # IO thread to display UI
+        t = Thread(target=recvAndRefresh, args=(client, ai, id))
+        t.start()
+
+        for c in cycle(gContext["steps"]):
+            if gContext["gameBeginFlag"]:
+                break
+            print(
+                f"\r\033[0;32m{c}\033[0m \33[1mWaiting for the other player to connect...\033[0m",
+                flush=True,
+                end="",
+            )
+            sleep(0.1)
+        # gracefully shutdown
+        t.join()
+
+
+if __name__ == "__main__":
+    main(1)
